29523c67bf18f3c0ba44cf99adcc7419eae59783
ismlslck <ismlslcl@gmail.com>
2018-02-12 01:38:57 +0300

    v1-login&register tasarimlari ve class'lari yazildi

---
+++ app/src/main/java/com/dashboard/telekrip/RegisterActivity.java
@@ -0,0 +1,49 @@
+package com.dashboard.telekrip;
+
+import android.app.Activity;
+import android.app.ActivityOptions;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class RegisterActivity extends Activity {
+    EditText _etUsername,_etPassword,_etRepeatPassword;
+    Button _btnRegister;
+    FloatingActionButton _fabLogin;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        uiInitialization();
+    }
+
+    private void uiInitialization() {
+        _etUsername=findViewById(R.id.etUsername);
+        _etPassword=findViewById(R.id.etPassword);
+        _etRepeatPassword=findViewById(R.id.etRepeatPassword);
+        _btnRegister=findViewById(R.id.btnRegister);
+        _fabLogin=findViewById(R.id.fabLogin);
+
+        _fabLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getWindow().setExitTransition(null);
+                getWindow().setEnterTransition(null);
+
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    ActivityOptions options =
+                            ActivityOptions.makeSceneTransitionAnimation(RegisterActivity.this, _fabLogin, _fabLogin.getTransitionName());
+                    startActivityForResult(new Intent(getApplicationContext(), LoginActivity.class), 2000, options.toBundle());
+                } else {
+                    startActivityForResult(new Intent(getApplicationContext(), LoginActivity.class), 2000);
+                }
+            }
+        });
+    }
+}
